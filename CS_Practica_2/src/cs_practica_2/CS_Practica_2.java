0bde1104be6dab103c9901245042e2297b471dd0
Fº Javier Gomez del Olmo <fjolmo@correo.ugr.es>
2013-11-09 22:15:43 +0100

    Sigue

--- CS_Practica_2/src/cs_practica_2/CS_Practica_2.java
+++ CS_Practica_2/src/cs_practica_2/CS_Practica_2.java
@@ -15,7 +15,7 @@ public class CS_Practica_2 {
      * @param complejoB numero complejo que se va a multiplicar.
      * @return numero complejo solucion.
      */
-    public static ArrayList<Double> mult_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
+    public static ArrayList<Double> mult_en_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
         ArrayList<Double> complejoC=new ArrayList<Double>();
              
         complejoC.add(0, (complejoA.get(0)*complejoB.get(0)) - (complejoA.get(1)*complejoB.get(1)));
@@ -31,7 +31,7 @@ public class CS_Practica_2 {
      * @param complejoB numero complejo que se va a sumar.
      * @return numero complejo solucion.
      */
-    public static ArrayList<Double> sum_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
+    public static ArrayList<Double> sum_en_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
         ArrayList<Double> complejoC=new ArrayList<Double>();
              
         complejoC.add(0,complejoA.get(0)+complejoB.get(0));
@@ -47,7 +47,7 @@ public class CS_Practica_2 {
      * @param complejoB numero complejo que se va a restar.
      * @return numero complejo solucion.
      */
-    public static ArrayList<Double> rest_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
+    public static ArrayList<Double> rest_en_C(ArrayList<Double> complejoA, ArrayList<Double> complejoB){
         ArrayList<Double> complejoC=new ArrayList<Double>();
              
         complejoC.add(0,complejoA.get(0)-complejoB.get(0));
@@ -81,47 +81,72 @@ public class CS_Practica_2 {
                ArrayList<Double> complejoA =  polA.get(i);
                ArrayList<Double> complejoB =  polB.get(k);
                ArrayList<Double> complejoC = polC.get(i+k);
-               ArrayList<Double> multC = mult_C(complejoA, complejoB);
-               polC.add(i+k, sum_C(multC, complejoC));
+               ArrayList<Double> multC = mult_en_C(complejoA, complejoB);
+               polC.add(i+k, sum_en_C(multC, complejoC));
             }
         }
         return polC;
     }
     
-
+    /**
+     * 
+     * @param N
+     * @param omega
+     * @param polA
+     * @return 
+     */
     public static ArrayList<ArrayList<Double>> FFT_en_C(Integer N, Integer omega, ArrayList<ArrayList<Double>> polA){
         ArrayList<ArrayList<Double>> A=new ArrayList<ArrayList<Double>>();
         
         return A;
     }
     
-    
+    /**
+     * 
+     * @param N
+     * @param omega
+     * @param polA
+     * @return 
+     */
     public static ArrayList<ArrayList<Double>> IFFT_en_C(Integer N, Integer omega, ArrayList<ArrayList<Double>> polA){
         ArrayList<ArrayList<Double>> A=new ArrayList<ArrayList<Double>>();
         
         return A;
     }
     
-    
+    /**
+     * 
+     * @param polA
+     * @param polB
+     * @param m
+     * @param n
+     * @return 
+     */
     public static ArrayList<ArrayList<Double>> multiplicacion_FFT_en_C(ArrayList<Integer> polA, ArrayList<Integer> polB, Integer m, Integer n){
         ArrayList<ArrayList<Double>> polC=new ArrayList<ArrayList<Double>>();
         
         return polC;
     }
     
-    
-    public static void mostrar_polinomio(ArrayList<Integer> pol){
+    /**
+     * Muestra por pantalla el polinomio de complejos empezando por el coeficiente menos significativo y mostrando entre
+     * parentesis la parte real (a la izquierda) y la parte imaginaria (a la derecha).
+     * @param pol el polinomio de complejos que se va a mostrar por pantalla 
+     */
+    public static void mostrar_polinomio_en_C(ArrayList<ArrayList<Double>> pol){
         for(int i=0;i<pol.size();i++)
-            System.out.print(pol.get(i)+"x^"+i+" + ");
+            System.out.print("("+pol.get(i).get(0)+", "+pol.get(i).get(1)+") ");
+        System.out.print("\n");
    }
     
     
    /**
-    * Lee de un fichero de entrada o de la entrada estándar un polinomio de numeros complejos
+    * Lee de la entrada estándar un polinomio de numeros complejos con el formato (real1,imag1)(real2,imag2)(real3,imag3)......
     * @return polinomio de numeros complejo
     */
-   public static ArrayList<Integer> leer_polinomio(){
-               InputStreamReader isr = new InputStreamReader(System.in);
+   public static ArrayList<ArrayList<Double>> leer_polinomio_en_C(){
+                ArrayList<ArrayList<Double>> pol=new ArrayList<ArrayList<Double>>();
+                InputStreamReader isr = new InputStreamReader(System.in);
                 BufferedReader br = new BufferedReader (isr); 
                 String input=new String();
                 String numStr;
@@ -131,28 +156,68 @@ public class CS_Practica_2 {
             System.out.print("\nExcepcion ocurrida "+e.getMessage());     
         }
         numStr=input;
-        Character numChar;
-        ArrayList<Integer> pol=new ArrayList<Integer>();
-        for(int i=0;i<numStr.length();i++){       
-            numChar=numStr.charAt(i);
-            pol.add(Integer.parseInt(numChar.toString()));
+        int i=0;
+        while(i<numStr.length()){       
+            if(numStr.charAt(i)=='('){
+                ArrayList<Double>complejo=new ArrayList<>();
+                i++;
+                String real="";
+                while(numStr.charAt(i)!=',' && i<numStr.length()){
+                   real+=numStr.charAt(i);
+                   i++;
+                }
+                Double valor=Double.parseDouble(real);
+                complejo.add(0,valor);
+                
+                i++;
+                String imag="";
+                while(numStr.charAt(i)!=')' && i<numStr.length()){
+                   imag+=numStr.charAt(i);
+                   i++;
+                }
+                valor=Double.parseDouble(imag);
+                complejo.add(1,valor);
+                
+                pol.add(complejo);
+            }
+            else
+                i++;
         }
         return pol;
    }
    
+   
+   //***************OPERACIONES PARA NUMEROS EN Z_41******************
+   
+   
+   
+   
+   
+   
+   //***************OPERACIONES PARA NUMEROS EN Z_257******************
+   
+   
+   
+   
+   
+   //***************OPERACIONES PARA NUMEROS EN Z_p********************
+   
     
+   
+   
     public static void main(String[] args) {
         
-        ArrayList<Integer> polA=new ArrayList<>();
-        ArrayList<Integer> polB=new ArrayList<>();
-        ArrayList<Integer> polC=new ArrayList<>();
+        ArrayList<ArrayList<Double>> polA=new ArrayList<ArrayList<Double>>();
+        ArrayList<ArrayList<Double>> polB=new ArrayList<ArrayList<Double>>();
+        ArrayList<ArrayList<Double>> polC=new ArrayList<ArrayList<Double>>();
          
 
-        System.out.print("\nINTRODUCE EL PRIMER POLINOMIO\n");
-        polA=leer_polinomio();
-        System.out.print("\nINTRODUCE EL SEGUNDO POLINOMIO\n");
-        polB=leer_polinomio();            
+        System.out.print("\nINTRODUCE EL PRIMER POLINOMIO EN C\n");
+        polA=leer_polinomio_en_C();
+        System.out.print("\nINTRODUCE EL SEGUNDO POLINOMIO EN C\n");
+        polB=leer_polinomio_en_C();            
        
-        //mostrar_polinomio(polC);
+        polC=multiplicacion_escuela_en_C(polA, polB);
+        mostrar_polinomio_en_C(polC);
     }
 }
